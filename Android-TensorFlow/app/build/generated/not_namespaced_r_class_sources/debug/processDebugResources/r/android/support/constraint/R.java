6307aa53369e0170ff40757035ae03aba206e0f0
wnddkdpcqkd <wnddkdpcqkd@gmail.com>
2019-05-19 19:36:25 +0900

    지도까지 됬음

--- Android-TensorFlow/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ Android-TensorFlow/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080033;
-        public static final int end = 0x7f08006f;
-        public static final int gone = 0x7f08007c;
-        public static final int invisible = 0x7f08009a;
-        public static final int left = 0x7f08009e;
-        public static final int packed = 0x7f0800d7;
-        public static final int parent = 0x7f0800d8;
-        public static final int percent = 0x7f0800da;
-        public static final int right = 0x7f0800ef;
-        public static final int spread = 0x7f08010e;
-        public static final int spread_inside = 0x7f08010f;
-        public static final int start = 0x7f080114;
-        public static final int top = 0x7f080136;
-        public static final int wrap = 0x7f080140;
+        public static final int end = 0x7f080070;
+        public static final int gone = 0x7f08007d;
+        public static final int invisible = 0x7f08009b;
+        public static final int left = 0x7f08009f;
+        public static final int packed = 0x7f0800d8;
+        public static final int parent = 0x7f0800d9;
+        public static final int percent = 0x7f0800db;
+        public static final int right = 0x7f0800f0;
+        public static final int spread = 0x7f08010f;
+        public static final int spread_inside = 0x7f080110;
+        public static final int start = 0x7f080115;
+        public static final int top = 0x7f080137;
+        public static final int wrap = 0x7f080141;
     }
     public static final class styleable {
         private styleable() {}
--- Android-TensorFlow/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/coreutils/R.java
+++ Android-TensorFlow/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/coreutils/R.java
@@ -85,25 +85,25 @@ public final class R {
         public static final int actions = 0x7f080017;
         public static final int async = 0x7f080028;
         public static final int blocking = 0x7f080030;
-        public static final int chronometer = 0x7f080054;
-        public static final int forever = 0x7f08007a;
-        public static final int icon = 0x7f080089;
-        public static final int icon_group = 0x7f08008a;
-        public static final int info = 0x7f080093;
-        public static final int italic = 0x7f08009b;
-        public static final int line1 = 0x7f0800a0;
-        public static final int line3 = 0x7f0800a1;
-        public static final int normal = 0x7f0800cf;
-        public static final int notification_background = 0x7f0800d0;
-        public static final int notification_main_column = 0x7f0800d1;
-        public static final int notification_main_column_container = 0x7f0800d2;
-        public static final int right_icon = 0x7f0800f0;
-        public static final int right_side = 0x7f0800f1;
-        public static final int tag_transition_group = 0x7f080121;
-        public static final int text = 0x7f080126;
-        public static final int text2 = 0x7f080128;
-        public static final int time = 0x7f080130;
-        public static final int title = 0x7f080131;
+        public static final int chronometer = 0x7f080055;
+        public static final int forever = 0x7f08007b;
+        public static final int icon = 0x7f08008a;
+        public static final int icon_group = 0x7f08008b;
+        public static final int info = 0x7f080094;
+        public static final int italic = 0x7f08009c;
+        public static final int line1 = 0x7f0800a1;
+        public static final int line3 = 0x7f0800a2;
+        public static final int normal = 0x7f0800d0;
+        public static final int notification_background = 0x7f0800d1;
+        public static final int notification_main_column = 0x7f0800d2;
+        public static final int notification_main_column_container = 0x7f0800d3;
+        public static final int right_icon = 0x7f0800f1;
+        public static final int right_side = 0x7f0800f2;
+        public static final int tag_transition_group = 0x7f080122;
+        public static final int text = 0x7f080127;
+        public static final int text2 = 0x7f080129;
+        public static final int time = 0x7f080131;
+        public static final int title = 0x7f080132;
     }
     public static final class integer {
         private integer() {}
